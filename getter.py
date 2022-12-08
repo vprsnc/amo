b31616b1d731e12d09ad1e253ad69e88783cc59f
Georgy <heavypresence@riesup.net>
2022-12-08 14:32:23 +0300

    debugging entitity

--- getter.py
+++ getter.py
@@ -73,14 +73,13 @@ def get_entity(entity, logon_data, amo, entity_subtype=None, filters=None, code=
     if session:
         logger.success("Successfully built session!")
 
-
         r = request_entities(
             url=build_url(logon_data, entity, filters if filters else None),
             session=session
         )
 # NOTE it's all ok with r, it returns the correct dict
 
-        contents = build_contents(r, entity_true_name)
+        contents = build_contents(r, entity)
         print(contents)
         write_contents(
             entity_true_name,
