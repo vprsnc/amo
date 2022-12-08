c53dc7144f8f139ec5387a02b35d8f047d52f79d
Georgy <heavypresence@riesup.net>
2022-12-08 13:53:04 +0300

    debugging url

--- getter.py
+++ getter.py
@@ -77,7 +77,7 @@ def get_entity(entity, logon_data, amo, entity_subtype=None, filters=None, code=
             url=build_url(logon_data, entity, filters if filters else None),
             session=session
         )
-        print(r.text())
+        print(r.text)
 
         write_contents(
             entity_true_name,
