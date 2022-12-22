a69fcb0f1e7b9debf2e65ba75517eff28e6e2f9b
Georgy <heavypresence@riesup.net>
2022-12-22 17:24:09 +0300

    namedtuple inheritance tool

--- utilities.py
+++ utilities.py
@@ -1,4 +1,6 @@
 import json
+from collections import namedtuple
+
 from datetime import datetime
 from loguru import logger
 
@@ -6,14 +8,18 @@ from loguru import logger
 
 def timer_decorator(func):
 
-     def timer(*args, **kwargs):
-         start = datetime.now()
-         logger.info(f'Starting timer at {start}')
-         func(*args, **kwargs)
-         end = datetime.now()
-         logger.success(f'Ending timer at {end}, it took {end-start}.')
+    def timer(*args, **kwargs):
+        start = datetime.now()
+        logger.info(f'Starting timer at {start}')
+        func(*args, **kwargs)
+        end = datetime.now()
+        logger.success(f'Ending timer at {end}, it took {end-start}.')
 
-     return timer
+    return timer
 
 def read_json(file):
-     return json.loads('[' + file.read()[:-2] + ']')
+    return json.loads('[' + file.read()[:-2] + ']')
+
+
+def inherit_named_tuple(new_tuple, old_tuple, new_fields):
+    return namedtuple(new_tuple, old_tuple._fields+(new_fields))
